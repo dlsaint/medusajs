e0723c0b27153c5de56c21fd28d607b3be08f03c
sainthe <sainthe@apifox.com>
2023-09-05 01:45:01 +0800

    feat: medusa project

---
+++ backend/develop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#sleep 1000000
+medusa migrations run
+
+medusa $1
---
+++ storefront/entrypoint-frontend.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [[ -z "${CI}" ]]; then
+    npx next dev -p 8000
+else
+    npx next build && npx next start -p 8000
+fi
