17cbdc75b9a71af3efeba6ec1041c455e125d709
sainthe <sainthe@apifox.com>
2023-09-05 22:54:52 +0800

    feat: medusa project

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -2,96 +2,97 @@ const dotenv = require("dotenv");
 
 let ENV_FILE_NAME = "";
 switch (process.env.NODE_ENV) {
-  case "production":
-    ENV_FILE_NAME = ".env.production";
-    break;
-  case "staging":
-    ENV_FILE_NAME = ".env.staging";
-    break;
-  case "test":
-    ENV_FILE_NAME = ".env.test";
-    break;
-  case "development":
-  default:
-    ENV_FILE_NAME = ".env";
-    break;
+    case "production":
+        ENV_FILE_NAME = ".env.production";
+        break;
+    case "staging":
+        ENV_FILE_NAME = ".env.staging";
+        break;
+    case "test":
+        ENV_FILE_NAME = ".env.test";
+        break;
+    case "development":
+    default:
+        ENV_FILE_NAME = ".env";
+        break;
 }
 
 try {
-  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
-} catch (e) {}
+    dotenv.config({path: process.cwd() + "/" + ENV_FILE_NAME});
+} catch (e) {
+}
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://192.168.10.208:7000,http://192.168.10.208:7001";
+    process.env.ADMIN_CORS || "http://192.168.10.208:7000,http://192.168.10.208:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
 // const STORE_CORS = process.env.STORE_CORS || "http://192.168.10.208:8000";
 const STORE_CORS = "*";
 
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+    process.env.DATABASE_URL || "postgres://localhost/medusa-store";
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
 const plugins = [
-  `medusa-fulfillment-manual`,
-  `medusa-payment-manual`,
-  {
-    resolve: `@medusajs/file-local`,
-    options: {
-      upload_dir: "uploads",
+    `medusa-fulfillment-manual`,
+    `medusa-payment-manual`,
+    {
+        resolve: `@medusajs/file-local`,
+        options: {
+            upload_dir: "uploads",
+        },
     },
-  },
-  {
-    resolve: "@medusajs/admin",
-    /** @type {import('@medusajs/admin').PluginOptions} */
-    options: {
-      serve: true,
-      path: "/app1",
-      autoRebuild: false,
-      develop: {
-        open: false,
-      },
-      port: 7001
-      // backend:"http://12.192.162.1:9001"
+    {
+        resolve: "@medusajs/admin",
+        /** @type {import('@medusajs/admin').PluginOptions} */
+        options: {
+            serve: true,
+            path: "/app1",
+            autoRebuild: true,
+            develop: {
+                open: false,
+            },
+            port: 7001,
+            backend: "http://12.192.162.1:9001"
+        },
+        // define: {
+        //   __MEDUSA_BACKEND_URL__: "12.192.162.1:9001",
+        //   MEDUSA_BACKEND_URL: "12.192.162.1:9001"
+        // }
     },
-    // define: {
-    //   __MEDUSA_BACKEND_URL__: "12.192.162.1:9001",
-    //   MEDUSA_BACKEND_URL: "12.192.162.1:9001"
-    // }
-  },
 ];
 
 const modules = {
-  /*eventBus: {
-    resolve: "@medusajs/event-bus-redis",
-    options: {
-      redisUrl: REDIS_URL
-    }
-  },
-  cacheService: {
-    resolve: "@medusajs/cache-redis",
-    options: {
-      redisUrl: REDIS_URL
-    }
-  },*/
+    /*eventBus: {
+      resolve: "@medusajs/event-bus-redis",
+      options: {
+        redisUrl: REDIS_URL
+      }
+    },
+    cacheService: {
+      resolve: "@medusajs/cache-redis",
+      options: {
+        redisUrl: REDIS_URL
+      }
+    },*/
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
-  jwtSecret: process.env.JWT_SECRET,
-  cookieSecret: process.env.COOKIE_SECRET,
-  store_cors: STORE_CORS,
-  database_url: DATABASE_URL,
-  admin_cors: ADMIN_CORS,
-  // Uncomment the following lines to enable REDIS
-  // redis_url: REDIS_URL
+    jwtSecret: process.env.JWT_SECRET,
+    cookieSecret: process.env.COOKIE_SECRET,
+    store_cors: STORE_CORS,
+    database_url: DATABASE_URL,
+    admin_cors: ADMIN_CORS,
+    // Uncomment the following lines to enable REDIS
+    // redis_url: REDIS_URL
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
 module.exports = {
-  projectConfig,
-  plugins,
-  modules,
+    projectConfig,
+    plugins,
+    modules,
 };
