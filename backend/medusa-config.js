37521927d63060a81c1fb8866a2c1c72786d23b2
sainthe <sainthe@apifox.com>
2023-09-05 16:46:17 +0800

    feat: medusa project

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -46,7 +46,7 @@ const plugins = [
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
-      // path: "app1",
+      path: "/app",
       autoRebuild: true,
       develop: {
         open: process.env.OPEN_BROWSER !== "false",
