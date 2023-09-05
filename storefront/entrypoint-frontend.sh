e10b2b75c9cfbfbdcb4c3ee5b8fcb9e46b2947e7
sainthe <sainthe@apifox.com>
2023-09-05 23:26:38 +0800

    feat: medusa project

--- storefront/entrypoint-frontend.sh
+++ storefront/entrypoint-frontend.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [[ -z "${CI}" ]]; then
-    npx next dev -p 8000
-else
-    npx next build && npx next start -p 8000
-fi
+npx next build && npx next start -p 8000
+
+#if [[ -z "${CI}" ]]; then
+#    npx next dev -p 8000
+#else
+#    npx next build && npx next start -p 8000
+#fi
