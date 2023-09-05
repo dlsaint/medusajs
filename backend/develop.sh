905de39675b35fecd42e495a197f9d8b32a2fa16
sainthe <sainthe@apifox.com>
2023-09-05 22:53:47 +0800

    feat: medusa project

--- backend/develop.sh
+++ backend/develop.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #sleep 1000000
-echo 'faaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
-node ./node_modules/\@medusajs/admin/bin/medusa-admin.js build
+#node ./node_modules/\@medusajs/admin/bin/medusa-admin.js build
 medusa migrations run
 medusa $1
